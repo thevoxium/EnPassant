050741e296273101d422a848a72ba5b9c8216f76
thevoxium <thevoxium@gmail.com>
2025-10-15 19:17:16 +0530

    update

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 #include "src/parser.h"
 
 int main() {
-  const char *fen = "r2qkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+  const char *fen =
+      "r3r1k1/pp1n1ppp/3bp3/3p4/3P1B2/2N1P3/PP3PPP/R2Q1RK1 w - - 0 21";
 
   Board *board = parseFen(fen);
   if (board == NULL) {
